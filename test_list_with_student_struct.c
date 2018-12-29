1330ea000a286177b010d7693a9adfae17684044
Nikos Giachoudis <ngiachou@uth.gr>
2018-12-29 17:46:16 +0200

    implements reading student information in a loop

--- test_list_with_student_struct.c
+++ test_list_with_student_struct.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "src/output_helpers.h"
@@ -16,9 +17,51 @@ int main(int argc, char* argv[])
 {
     List student_list;
 
+    init_list(&student_list);
+
     get_input(&student_list);
 
     print_from_list(student_list, &print_student);
+}
+
+void get_input(List *a_list)
+{
+    int menu_choice, id;
+    char buf[256];
+
+    Student *new_student;
+
+    printf("1. Insert new student\n0. Exit\n\n");
+    scanf("%d", &menu_choice);
+
+    while(menu_choice != 0)
+    {
+        if(menu_choice == 1)
+        {
+            new_student = (Student*) malloc(sizeof(Student));
+
+            // read id
+            printf("Please give student's ID:\n");
+            scanf("%d", &id);
+            new_student->id = id;
+
+            // read first name
+            printf("Please give student's first name:\n");
+            scanf("%s", buf);
+            new_student->first_name = (char*) malloc(strlen(buf));
+            memcpy(new_student->first_name, buf, strlen(buf));
+
+            // read last name
+            printf("Please give student's last name:\n");
+            scanf("%s", buf);
+            new_student->last_name = (char*) malloc(strlen(buf));
+            memcpy(new_student->last_name, buf, strlen(buf));
+        }
+
+        printf("DONE!\n\n");
+        printf("1. Insert new student\n0. Exit\n\n");
+        scanf("%d", &menu_choice);
+    }    
 }
 
 void print_student(const void* student)
